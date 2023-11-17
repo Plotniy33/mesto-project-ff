3c1c0ff7ab7ffba91846e7d9c85dc7fc6023f0f9
Plotniy33 <plotnikoffvolodimir@gmail.com>
2023-11-17 20:03:28 +0300

    запихнул кнопку удаление карточки в константу, добавил ariaLabel к кнопкам, повесил defer на скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -9,30 +9,27 @@ const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 
 function createCard(el, delCard) {
-const cardElement = cardTemlate.querySelector('.card').cloneNode(true);
-  cardImg = cardElement.querySelector('.card__image').setAttribute('src', el.link);
-  cardImg = cardElement.querySelector('.card__image').setAttribute('alt', el.name);
-  cardTitle = cardElement.querySelector('.card__title').textContent = el.name;
-  btnDel = cardElement.querySelector('.card__delete-button');
-    
+  const cardElement = cardTemlate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').setAttribute('src', el.link);
+    cardElement.querySelector('.card__image').setAttribute('alt', el.name);
+    cardElement.querySelector('.card__title').textContent = el.name;
 
-  btnDel.addEventListener('click', delCard);
+  const btnDel = cardElement.querySelector('.card__delete-button');
+    btnDel.addEventListener('click', delCard);
+
   return cardElement;
 }
-
 
 // @todo: Функция удаления карточки
 
 function delCard(evt) {
-    const item = evt.target.closest('.card');
-    item.remove();
+  const item = evt.target.closest('.card');
+  item.remove();
 }
 
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach(function(cardElement) {
+initialCards.forEach(function (cardElement) {
   const card = createCard(cardElement, delCard);
   placesList.append(card);
 });
-
-
