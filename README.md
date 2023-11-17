30da5593beeeccc5c1aa20a963d2f20ef52dff1c
Plotniy33 <plotnikoffvolodimir@gmail.com>
2023-11-17 12:40:31 +0300

    сдаем на первую проверку

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+https://github.com/Plotniy33/mesto-project-ff.git
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
 
+const cardTemlate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function createCard(el, delCard) {
+const cardElement = cardTemlate.querySelector('.card').cloneNode(true);
+  cardImg = cardElement.querySelector('.card__image').setAttribute('src', el.link);
+  cardImg = cardElement.querySelector('.card__image').setAttribute('alt', el.name);
+  cardTitle = cardElement.querySelector('.card__title').textContent = el.name;
+  btnDel = cardElement.querySelector('.card__delete-button');
+    
+
+  btnDel.addEventListener('click', delCard);
+  return cardElement;
+}
+
+
 // @todo: Функция удаления карточки
 
+function delCard(evt) {
+    const item = evt.target.closest('.card');
+    item.remove();
+}
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function(cardElement) {
+  const card = createCard(cardElement, delCard);
+  placesList.append(card);
+});
+
+
